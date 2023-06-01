d7538b4f6f3a3da1130c08b58d910a3bf8e1c1d7
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-01 11:07:52 -0500

    uploading more progress

--- typescript-basics/app.js
+++ typescript-basics/app.js
@@ -1,47 +1,26 @@
-//void type
-// function add(n1: number, n2: number) {
-//   return n1 + n2;
+// //unknown types
+// let userInput: unknown; //could technically hold any value
+// let userName: string;
+// userInput = 5;
+// userInput = "Me";
+// if (typeof userInput === "string") {
+//   //need to do an extra type check to be able to assign the uknown to string var
+//   userName = userInput;
 // }
-// //this function doesnt return anything so it is void
-// //use void and not undefined if function doesn't return anything
-// function printResult(num: number): void {
-//   console.log("Result: " + num);
-// }
-// printResult(add(5, 12));
-//undefined is a valid type in TS
-// let someValue: undefined;
-//functions as types
-// function add(n1: number, n2: number) {
-//   return n1 + n2;
-// }
-// //this function doesnt return anything so it is void
-// //use void and not undefined if function doesn't return anything
-// function printResult(num: number): void {
-//   console.log("Result: " + num);
-// }
-// printResult(add(5, 12));
-// //makes it clear that whatever we store in there is going to be a function
-// // let combineValues: Function;
-// //below we are saying that combineValues should take a function where the return type is a number the 2 parameters that are a number. syntax in arrow function
-// let combineValues: (a: number, b: number) => number;
-// combineValues = add;
-// // combineValues = printResult;
-// console.log(combineValues(8, 8));
-//function types--callbacks
-function add(n1, n2) {
-    return n1 + n2;
+// //cant do this
+// // userName = userInput;
+//the never type
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Me";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+function generateErr(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateErr("uh oh spaghettios!", 500);
--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -1,14 +1,36 @@
-//unknown types
+// //unknown types
 
+// let userInput: unknown; //could technically hold any value
+// let userName: string;
+
+// userInput = 5;
+// userInput = "Me";
+// if (typeof userInput === "string") {
+//   //need to do an extra type check to be able to assign the uknown to string var
+//   userName = userInput;
+// }
+
+// //cant do this
+// // userName = userInput;
+
+//the never type
 let userInput: unknown;
 let userName: string;
 
 userInput = 5;
 userInput = "Me";
 if (typeof userInput === "string") {
-  //need to do an extra type check to be able to assign the uknown to string var
   userName = userInput;
 }
 
-//cant do this
-// userName = userInput;
+function generateErr(message: string, code: number): never {
+  throw {
+    message,
+    errorCode: code,
+  };
+  //infinite loops also never return anything
+  //while(true) {}
+}
+
+//this function never returns anything and will crash script unless used with try/catch
+generateErr("uh oh spaghettios!", 500);
