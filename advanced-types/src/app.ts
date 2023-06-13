9e43cc93b71528bc52d7bf1f2018f2a205b5f925
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-12 21:15:10 -0500

    module has finished

--- advanced-types/src/app.ts
+++
@@ -1,8 +0,0 @@
-//Nullish Coalescing
-
-const userInput = null;
-
-//just like in regular JS. If value is null, go to the second value or 'DEFAULT' in this case. Thats only if the value is null, not undefined or an empty string, but null
-const storedData = userInput ?? "DEFAULT";
-
-console.log(storedData);
