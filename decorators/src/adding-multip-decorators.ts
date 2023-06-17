4a2d437171904664919d6dcd7685522ea6f5b44a
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-17 18:52:15 -0500

    Add Multiple Decorators

---
+++ decorators/src/adding-multip-decorators.ts
@@ -0,0 +1,38 @@
+//Adding Multiple Decorators
+
+function Logger(logString: string) {
+  console.log("1");
+  return function (constructor: Function) {
+    console.log("4");
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  console.log("2");
+  return function (constructor: any) {
+    console.log("3");
+    console.log("rendering template");
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+//the decorator factories run first from top to bottom, then the deorators themselves run from bottom to top.
+
+@Logger("Logging Person")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Miklo";
+
+  constructor() {
+    console.log("Creating person");
+  }
+}
+
+const person1 = new Person();
