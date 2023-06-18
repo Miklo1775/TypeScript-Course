2e337039c1cad4b64697c504eb04d5a0db9bf108
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-17 20:53:54 -0500

    Example with an Autobind Function

--- decorators/src/accessor-parameter-decorators.ts
+++ decorators/src/accessor-parameter-decorators.ts
@@ -1,56 +1,56 @@
 //Accesor Parameter Decorators
 
-function Log(target: any, propertyName: string | Symbol) {
-  console.log("Property Decorator");
-  console.log(target, propertyName);
-}
+// function Log(target: any, propertyName: string | Symbol) {
+//   console.log("Property Decorator");
+//   console.log(target, propertyName);
+// }
 
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Accessor Decorator");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log("Accessor Decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
 
-function Log3(
-  target: any,
-  name: string | Symbol,
-  descriptor: PropertyDescriptor
-) {
-  console.log("Method Decorator");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
+// function Log3(
+//   target: any,
+//   name: string | Symbol,
+//   descriptor: PropertyDescriptor
+// ) {
+//   console.log("Method Decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
 
-function Log4(target: any, name: string | Symbol, position: number) {
-  console.log("Paramter Decorator");
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
+// function Log4(target: any, name: string | Symbol, position: number) {
+//   console.log("Paramter Decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(position);
+// }
 
-class Product {
-  @Log
-  title: string;
-  private _price: number;
+// class Product {
+//   @Log
+//   title: string;
+//   private _price: number;
 
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error("Invalid price; Should be greater than 0.");
-    }
-  }
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0) {
+//       this._price = val;
+//     } else {
+//       throw new Error("Invalid price; Should be greater than 0.");
+//     }
+//   }
 
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this._price = p;
+//   }
 
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this._price * (1 + tax);
-  }
-}
+//   @Log3
+//   getPriceWithTax(@Log4 tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
