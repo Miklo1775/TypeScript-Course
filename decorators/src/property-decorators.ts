b685f2db2d3b3751824e0e6c0ad37bb6ef1b2e7b
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-17 19:20:33 -0500

    Property Decorators

---
+++ decorators/src/property-decorators.ts
@@ -0,0 +1,32 @@
+//Property Decorators
+
+//adding decorator for a property in a class
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property Decorator");
+  //returns the prototype and the name of the property
+  console.log(target, propertyName);
+}
+
+class Product {
+  // property decorator runs when the property is defined in JS
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price; Should be greater than 0.");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
