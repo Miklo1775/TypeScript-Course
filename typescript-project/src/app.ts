06c1f8fa928d1ecce212ecdba55b67b60bc64351
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-19 22:37:06 -0500

    adding a getter to ProjectItem class

--- typescript-project/src/app.ts
+++ typescript-project/src/app.ts
@@ -71,6 +71,14 @@ abstract class Base<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Base<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} people`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
     this.project = project;
@@ -81,7 +89,7 @@ class ProjectItem extends Base<HTMLUListElement, HTMLLIElement> {
   configure() {}
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent = this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
