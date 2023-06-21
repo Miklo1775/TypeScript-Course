1a8d8a2a46dde2be63086941b8ed40f5c55da910
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-06-20 21:43:09 -0500

    completed webpack module

---
+++ webpack/src/components/project-inputs.ts
@@ -0,0 +1,87 @@
+import { Base } from "./base";
+import { Validatable, validate } from "../util/validation";
+import { AutoBind } from "../decorators/autobind-decorator";
+import { projectState } from "../state/project-state";
+
+type AllOrNothing = [string, string, number] | void;
+
+export class ProjectInput extends Base<HTMLDivElement, HTMLFormElement> {
+  // templateEl: HTMLTemplateElement;
+  // hostEl: HTMLDivElement;
+  // element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+
+  constructor() {
+    super("project-input", "app", true, "user-input");
+
+    this.titleInputEl = <HTMLInputElement>this.element.querySelector("#title")!;
+
+    this.descriptionInputEl = <HTMLInputElement>this.element.querySelector("#description")!;
+
+    this.peopleInputEl = <HTMLInputElement>this.element.querySelector("#people")!;
+
+    this.configure();
+    // this.attach();
+  }
+
+  private clearInputs() {
+    this.titleInputEl.value = "";
+    this.descriptionInputEl.value = "";
+    this.peopleInputEl.value = "";
+  }
+
+  private gatherUserInput(): AllOrNothing {
+    const enteredTitle = this.titleInputEl.value;
+    const enteredDescription = this.descriptionInputEl.value;
+    const enteredPeople = +this.peopleInputEl.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+      alert("invalid input");
+      return;
+    } else {
+      console.log(validate(titleValidatable));
+
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  @AutoBind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      projectState.addProject(title, desc, people);
+      this.clearInputs();
+    }
+  }
+
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  // private attach() {
+  //   this.hostEl.insertAdjacentElement("afterbegin", this.element);
+  // }
+  renderContent() {}
+}
---
+++ webpack/src/components/project-item.ts
@@ -0,0 +1,42 @@
+import { Draggable } from "../models/drag-drop-interfaces";
+import { AutoBind } from "../decorators/autobind-decorator";
+import { Project } from "../models/project-model";
+import { Base } from "./base";
+export class ProjectItem extends Base<HTMLUListElement, HTMLLIElement> implements Draggable {
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} people`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+    this.configure();
+    this.renderContent();
+  }
+
+  @AutoBind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
+  @AutoBind
+  dragEndHandler(_: DragEvent) {
+    console.log("Drag has ended");
+  }
+
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragstart", this.dragEndHandler);
+  }
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
